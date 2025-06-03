6b595986943c171fef08209edeca41f180bb6750
-h <hermsenl@xs4all.nl>
2025-06-03 15:06:31 +0200

    Separate measurement function into measure.c/h

---
+++ include/measure.h
@@ -0,0 +1,7 @@
+#ifndef MEASURE_H
+#define MEASURE_H
+
+// functions
+void measure_data(SignalConfig_t *signals, int *measbuffer, int signalCount);
+
+#endif
---
+++ src/measure.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+
+#include "config.h"
+#include "measure.h"
+
+
+void measure_data(SignalConfig_t *signals, int *measbuffer, int signalCount)
+{    
+    uint8_t rawData = 0;
+    uint32_t elapsed_ms = 0;
+    int ssm_return = 0;
+
+    for (int pollIdx = 0; pollIdx < signalCount; pollIdx++) {
+        if (!signals[pollIdx].loggingEnabled) {
+            continue; //skip signal that is not to be logged
+        }
+
+        ssm_return = ssm_query_ecu(signals[pollIdx].address, &rawData, 1);
+
+        if (ssm_return != 0) {
+            continue; // failed read is skipped
+        }
+
+        // Update the buffer for this signal only
+        measbuffer[pollIdx] = ((rawData + signals[pollIdx].conversionOffset) * 
+                               signals[pollIdx].conversionMulFactor) / 
+                               signals[pollIdx].conversionDivFactor;
+        }
+}
+ 
